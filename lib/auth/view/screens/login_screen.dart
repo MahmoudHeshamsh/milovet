2bb7235389685a0c7d587996f0b0929d47b24209
Mahmoud Hesham <mshalash689@gmail.com>
2025-02-22 21:56:27 +0200

    Finish Splash screen & Get start screens

---
+++ lib/auth/view/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:milovet/shared/color_manager.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ColorManager.lightPurple,
+      ),
+    );
+  }
+}
---
+++ lib/shared/routes/routes.dart
@@ -0,0 +1,8 @@
+class Routes {
+  static const String onboardingOne = '/onboarding_one';
+  static const String onboardingTwo = '/onboarding_two';
+  static const String getStarted = '/get_started';
+  static const String login = '/login';
+  static const String register = '/register';
+
+}
