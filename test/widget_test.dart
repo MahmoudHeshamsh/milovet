256cb7cda63f5ee8ceee7eca84b2f261c4a8623e
Mahmoud Hesham <mshalash689@gmail.com>
2025-02-22 17:12:32 +0200

    Prepare the project to start

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:milovet/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MiloVet());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
