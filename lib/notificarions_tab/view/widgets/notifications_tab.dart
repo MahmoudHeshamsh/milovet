d040740f80334c4e5fe1847000dce3360f777d63
Mahmoud Hesham <mshalash689@gmail.com>
2025-02-23 22:35:50 +0200

    Finish notifications UI

--- lib/notificarions_tab/view/widgets/notifications_tab.dart
+++ lib/notificarions_tab/view/widgets/notifications_tab.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NotificationsTab extends StatelessWidget {
   const NotificationsTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Notifications',style: TextStyle(color: Colors.amber),),),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(height: 150.h,),
+        Image.asset('assets/images/notifications.png',height: 163.h,width: 163.w,),
+        SizedBox(height: 20.h,),
+        Center(child: Text('No New Notifications Right Now',style: Theme.of(context).textTheme.bodyLarge,))
+      ],
     );
   }
 }
